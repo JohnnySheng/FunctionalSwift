0c4b26f669e8bb9c3f5aa0fbbd021b0e452862dc
JohnnySheng <yuangangsheng@gmail>
2018-05-25 06:34:11 +0800

    the self defined type Region: Position -> Bool

--- SwiftLearning/ViewController.swift
+++ SwiftLearning/ViewController.swift
@@ -9,6 +9,37 @@
 import UIKit
 
 typealias Distance = Double
+typealias Region = (Position) -> Bool
+func circle(redius : Distance) -> Region {
+    return {
+        point in point.length < redius
+    }
+}
+func shift(_ region:@escaping Region, by offset: Position) -> Region {
+    return { point in region(point.minus(offset))}
+}
+
+func invert(_ region: @escaping Region) -> Region {
+    return { point in !region(point)}
+}
+
+func intersect(_ region: @escaping Region, with other:@escaping Region) -> Region {
+    return{ point in region(point) && other(point)}
+}
+
+func union(_ region: @escaping Region, with other: @escaping Region) -> Region{
+    return{ point in region(point) || other(point)}
+}
+
+func subtract(_ region: @escaping Region, from original: @escaping Region) -> Region {
+    return intersect(original, with: invert(region))
+}
+
+struct Region1 {
+    let lookup:(Position) -> Bool
+}
+//
+//
 struct Position{
     var x : Double
     var y : Double
@@ -24,11 +55,29 @@ extension Position{
     func minus(_ p : Position) -> Position {
         return Position(x: x - p.x, y: y - p.y)
     }
+    
+    func circle(redius : Distance) -> Region {
+        return {
+            point in point.length < redius
+        }
+    }
+    
+    func circle2(redius: Distance, center:Position) -> Region {
+        return {
+            point in point.minus(center).length < redius
+        }
+    }
+    
+    
+
+  
+
     var length : Double{
         return sqrt(x*x + y*y)
     }
-    
 }
+//
+//
 struct Ship {
     var position:Position
     var fireRange:Distance
@@ -43,6 +92,7 @@ extension Ship{
         return sqrt(dx*dx + dy*dy) <= fireRange
     }
 }
+
 extension Ship{
     func canSafelyEngage(ship target:Ship) -> Bool {
         let dx = target.position.x - position.x
@@ -67,14 +117,29 @@ extension Ship{
         friendlyDistance > unsafeRange
     }
 }
+
 extension Ship{
     func canSafelyEngage2(ship target:Ship, friendly:Ship) -> Bool {
+        
+//        let shifted = target.position.shift(circle(redius: 10), by:Position(x: 5, y:5))
+        
         let targetDistance = target.position.minus(position).length
         let friendlyDistance = friendly.position.minus(position).length
         
         return targetDistance <= fireRange &&
             targetDistance > unsafeRange &&
             friendlyDistance > unsafeRange
+    }
+}
+
+extension Ship{
+    func canSafelyEngage3(ship target: Ship, friendly:Ship) -> Bool {
+        let rangeRegion = subtract(circle(redius:unsafeRange), from: circle(redius: fireRange))
+        let fireRegion = shift(rangeRegion, by: position)
+        
+        let friendlyRegion = shift(circle(redius: unsafeRange), by: friendly.position)
+        let resultRegion = subtract(friendlyRegion, from: fireRegion)
+        return resultRegion(target.position)
     }
 }
 
@@ -89,6 +154,9 @@ class ViewController: UIViewController {
             var x : Double
             var y : Double
         }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
